f4e63d2a14d429b7814daf3ac2ff95ae6d304c0e
janessacodes <59347196+janessacodes@users.noreply.github.com>
2020-02-17 14:42:28 -0800

    Add files via upload

--- Robot.java
+++ Robot.java
@@ -7,15 +7,18 @@
 
 package frc.robot;
 
-// import static org.junit.Assert.assertFalse;
-import edu.wpi.first.wpilibj.Compressor;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-import edu.wpi.first.wpilibj.Joystick;
+import com.revrobotics.ColorMatch;
+import com.revrobotics.ColorMatchResult;
+import com.revrobotics.ColorSensorV3;
+import edu.wpi.first.wpilibj.DriverStation;
+
+import edu.wpi.first.wpilibj.I2C;
+//import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.drive.MecanumDrive;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.util.Color;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -30,19 +33,27 @@ public class Robot extends TimedRobot {
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
 
-  public WPI_TalonSRX frontLeft; //
-  public WPI_TalonSRX frontRight; //
-  public WPI_TalonSRX rearLeft; //
-  public WPI_TalonSRX rearRight; //
-  public WPI_TalonSRX clawLeft; //
-  public WPI_TalonSRX clawRight; //
+  /* private denotes that the class variables are not available in other classes.
+  the talons are objects */
 
-  public MecanumDrive robotDrive; //
+  private WPI_TalonSRX diskMotor;
 
-  public Joystick joystick; //
+  //private Joystick leftJoyStick;
+  //private Joystick rightJoyStick;
 
-  public Compressor c; //
-  public DoubleSolenoid solenoid; //
+  private final I2C.Port i2cPort = I2C.Port.kOnboard;
+  private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
+  private final ColorMatch m_colorMatcher = new ColorMatch();
+
+  private final Color kBlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
+  private final Color kGreenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
+  private final Color kRedTarget = ColorMatch.makeColor(0.561, 0.232, 0.114);
+  private final Color kYellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);
+
+  private Double insanityFactor = 0.5; /* set at half speed */
+
+  private boolean positionControl = false;
+  private boolean rotationControl = false;
 
   /**
    * This function is run when the robot is first started up and should be
@@ -53,27 +64,26 @@ public class Robot extends TimedRobot {
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
+/* code below instantiates the talons (which number)
+*/
+/* the code below instantiates the speed controller group so that we can control all
+the talons as a group (set all to speed 5, for ex) */
+    diskMotor = new WPI_TalonSRX(12);
 
-    frontLeft = new WPI_TalonSRX(1); //
-    frontRight = new WPI_TalonSRX(2); //
-    rearLeft = new WPI_TalonSRX(3); //
-    rearRight = new WPI_TalonSRX(4); //
-    clawLeft = new WPI_TalonSRX(8); //
-    clawRight = new WPI_TalonSRX(7); //
-    c = new Compressor(0);
-    solenoid = new DoubleSolenoid(2, 3); //
-    // }
-    
-    robotDrive = new MecanumDrive(frontLeft, rearLeft, frontRight, rearRight); // translating input
-    // that says i wanna go forward, i wanna go right to the direction of speed as to 
-    // spin the wheels
+    //leftJoyStick = new Joystick(0);
+    //rightJoyStick = new Joystick(1);
 
-    joystick = new Joystick(0); //
-    System.out.println(joystick.getRawButtonPressed(1));
+    m_colorMatcher.addColorMatch(kBlueTarget);
+    m_colorMatcher.addColorMatch(kGreenTarget);
+    m_colorMatcher.addColorMatch(kRedTarget);
+    m_colorMatcher.addColorMatch(kYellowTarget); 
 
-    c.setClosedLoopControl(true);
+    SmartDashboard.putNumber("insanityFactor", insanityFactor);
+    //on the dashboard, it will create a box of sort with title insanityfactor and display //insanity factor*/
+
+    SmartDashboard.putBoolean("Position control", positionControl);
+    SmartDashboard.putBoolean("Rotation control", rotationControl);
   }
-
   /**
    * This function is called every robot packet, no matter the mode. Use
    * this for items like diagnostics that you want ran during disabled,
@@ -81,8 +91,17 @@ public class Robot extends TimedRobot {
    *
    * <p>This runs after the mode specific periodic functions, but before
    * LiveWindow and SmartDashboard integrated updating.
+   */
+  @Override
+  public void robotPeriodic() {
+    
+    }
 
-  }
+    // SmartDashboard.putNumber("Red", detectedColor.red);
+    // SmartDashboard.putNumber("Green", detectedColor.green);
+    // SmartDashboard.putNumber("Blue", detectedColor.blue);
+    // SmartDashboard.putNumber("Confidence", match.confidence);
+    // SmartDashboard.putString("Detected Color", colorString);
 
   /**
    * This autonomous (along with the chooser code above) shows how to select
@@ -123,30 +142,73 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void teleopPeriodic() {
-    robotDrive.driveCartesian(joystick.getRawAxis(1), joystick.getRawAxis(0), joystick.getRawAxis(2)); //
-    if (joystick.getRawButton(5))
-    {
-      clawLeft.set(-0.5);
-      clawRight.set(0.5);
-    } else if (joystick.getRawButton(3))
-    {
-      clawLeft.set(0.5);
-      clawRight.set(-0.5);
-    } else
-    {
-      clawLeft.set(0);
-      clawRight.set(0);
+    insanityFactor = SmartDashboard.getNumber("insanityFactor", insanityFactor);
+//gets the value from insanityfactor. if it doesn't it'll set it to current insanity factor
+while(rotationControl) {
+  final Color initialColor = m_colorSensor.getColor();
+  //String initColorString;
+  final ColorMatchResult initialMatch = m_colorMatcher.matchClosestColor(initialColor);
+  if (initialMatch.color == kBlueTarget) {
+    //initColorString = "Blue";
+  } else if (initialMatch.color ==  kRedTarget) {
+    //initColorString = "Red";
+  } else if (initialMatch.color == kGreenTarget) {
+    //initColorString = "Green";
+  } else if (initialMatch.color == kYellowTarget) {
+    //initColorString = "Yellow";
+  } else {
+    //initColorString = "Unknown";
+  }
+  int colorSeen;
+  colorSeen = 0;
+  while (colorSeen < 8) {
+    diskMotor.set(0.5);
+    final Color detectedColor = m_colorSensor.getColor();
+    //final String detColorString;
+    final ColorMatchResult detectedMatch = m_colorMatcher.matchClosestColor(detectedColor);
+    while (detectedMatch.color == initialMatch.color) {
+      colorSeen = colorSeen+1;
     }
-    
-  if(joystick.getRawButtonPressed(7)) 
-  {
-    solenoid.set(DoubleSolenoid.Value.kForward);
   }
-  if(joystick.getRawButtonPressed(8))
+  diskMotor.set(0);
+Color targetColor = ColorMatch.makeColor(0, 0, 0);
+while(positionControl) {
+String gameData;
+gameData = DriverStation.getInstance().getGameSpecificMessage();
+if(gameData.length() > 0)
+{
+  switch (gameData.charAt(0))
   {
-    solenoid.set(DoubleSolenoid.Value.kReverse);
+    case 'B' :
+      targetColor = kBlueTarget;
+      break;
+    case 'G' :
+      targetColor = kGreenTarget;
+      break;
+    case 'R' :
+      targetColor = kRedTarget;
+      break;
+    case 'Y' :
+      targetColor = kYellowTarget;
+      break;
+    default :
+      //This is corrupt data
+      break;
+  // make a loop where the gear spins until detected color is the color from gameData
   }
-
+  //ColorMatchResult detMatch;
+  Color detColor = m_colorSensor.getColor();
+  while(detColor != targetColor) {
+    diskMotor.set(0.25);
+    detColor = m_colorSensor.getColor();
+    //detMatch = m_colorMatcher.matchClosestColor(detColor);
+  }
+  diskMotor.set(0);
+} else {
+  diskMotor.set(0);
+}
+}
+}
   }
 
   /**
